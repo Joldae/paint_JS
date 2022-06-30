03096c309c5ab79d457b5616e895b6f1737bfa9b
Joldae <76109507+Joldae@users.noreply.github.com>
2022-06-30 21:22:06 +0900

    commit 6.30

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
 const canvas = document.getElementById("JScanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+
+canvas.width = 700;
+canvas.height = 700;
 let painting = false;
+
+ctx.strokeStyle = "##2c2c2c";
+ctx.lineWidth = 2.5;
 
 function stopPainting() {
   painting = false;
 }
 
-function onMouseMove(event) {
-  const x = event.offsetX;
-  const y = event.offsetY;
-}
-
-function onMouseDown(event) {
+function startPainting() {
   painting = true;
 }
 
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    console.log("creating line in");
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+
+function onMouseDown(event) {
+  startPainting();
+}
+
 function onMouseUp(event) {
-  painting = false;
+  stopPainting();
+}
+function handleColorClick(evenr) {
+  console.log(event.target.style);
 }
 
 if (canvas) {
@@ -24,3 +47,7 @@ if (canvas) {
   canvas.addEventListener("mouseup", onMouseUp);
   canvas.addEventListener("mouseleave", stopPainting());
 }
+
+Array.from(colors).forEach((color) =>
+  color.addEventListener("click", handleColorClick)
+);
